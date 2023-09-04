4afe0504866648823b211f5b540becd703c70bcc
yesicazemanate <zunigahernandario@gmail.com>
2023-09-04 11:10:46 -0500

    clase 4 septiembre

--- miprimerjs/javaScript/practica1sep.js
+++ miprimerjs/javaScript/practica1sep.js
@@ -1,22 +1,79 @@
-let lista=document.querySelector(".lista");
+/*let lista=document.querySelector(".lista");
 let boton=document.querySelector(".boton");
-let agregar=document.querySelector(".textoAgregar")
+let eliminar=document.querySelector(".eliminar");
+let agregar
 
 let li
-let ul
-   
-  let funcion= window.onload=function(agregar) {
-    
+eliminar.addEventListener("click",()=>{
+lista.parentNode.removeChild(lista);
+
+  
+  })
+
+boton.addEventListener("click",()=>{
+  let funcion= window.onload=function() {
+     
     li =document.createElement("li");
     lista.appendChild(li)
      let text=document.createTextNode(agregar);
     li.appendChild(text);
         
    }
-
-boton.addEventListener("click",()=>{
-   funcion(agregar,
-    document.body.appendChild(lista))
+  agregar=document.querySelector("#textoAgregar").value;
+   funcion(document.body.appendChild(lista))
    
      
+})*//*
+let boton=document.querySelector(".empezar")
+let h1=document.querySelector(".h1")
+let contador=0
+
+
+
+boton.addEventListener("click",()=>{
+
+ const seguir=setInterval(()=>{
+      contador++
+      h1.textContent=contador 
+     if(contador>=50){
+      clearInterval(seguir)}
+    },200)
+  
+      
+    
+
+ 
+  
+});*/
+let boton=document.querySelector(".empezar")
+let horas=document.querySelector(".h1")
+let minutos=document.querySelector(".h2")
+let segundos=document.querySelector(".h3")
+let hours=0
+let minut=0
+let seconds=1
+boton.addEventListener("click",()=>{
+  const seguir=setInterval(()=>{
+   seconds++
+    segundos.textContent=seconds
+   if(seconds>=59){
+    minut++
+   minutos.textContent=minut
+    
+  seconds=0}
+  if(minut>=59){
+   hours++
+   horas.textContent=hours
+    
+ minut=0}
+
+  },5)
 })
+
+ 
+
+
+
+
+  
+
